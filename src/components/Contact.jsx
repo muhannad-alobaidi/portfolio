7bcb407fa9ae7e224cccb028dca51e5886d87a67
muhannadalobaidi <43783347+muhannadalobaidi@users.noreply.github.com>
2023-09-20 17:42:34 +0300

    updates

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
-
 const Contact = () => {
-  return <div>Contact</div>;
+  return <div>.</div>;
 };
 
 export default Contact;
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
-
 const Experience = () => {
-  return <div>Experience</div>;
+  return <div>.</div>;
 };
 
 export default Experience;
